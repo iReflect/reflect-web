44aef9c192d9d63f10f97a9b2f69419a6d19a017
Chetanya Kandhari <chetanya.kandhari@joshtechnologygroup.com>
2018-02-13 14:24:27 +0530

    [#IR-50] Review fixes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -22,7 +22,6 @@ import { AgGridModule } from 'ag-grid-angular';
 import { RatingEditorComponent } from './ag-grid-editors/rating-editor/rating-editor.component';
 import { NumericCellEditorComponent } from './ag-grid-editors/numeric-cell-editor/numeric-cell-editor.component';
 import { RatingRendererComponent } from './ag-grid-renderers/rating-renderer/rating-renderer.component';
-import { RatingRendererComponent } from './ag-grid-renderers/rating-renderer/rating-renderer.component';
 import { DeleteButtonRendererComponent } from './ag-grid-renderers/delete-button-renderer/delete-button-renderer.component';
 import { RetrospectButtonRendererComponent } from './ag-grid-renderers/retrospect-button-renderer/retrospect-button-renderer.component';
 
@@ -38,8 +37,7 @@ import { RetrospectButtonRendererComponent } from './ag-grid-renderers/retrospec
             RatingEditorComponent,
             NumericCellEditorComponent,
             RatingRendererComponent,
-            DeleteButtonRendererComponent
-            VacationRendererComponent,
+            DeleteButtonRendererComponent,
             RetrospectButtonRendererComponent,
         ])
     ],
@@ -50,9 +48,6 @@ import { RetrospectButtonRendererComponent } from './ag-grid-renderers/retrospec
         BasicModalComponent,
         RatingRendererComponent,
         RatingEditorComponent,
-        DeleteButtonRendererComponent,
-        NumericCellEditorComponent
-        VacationRendererComponent,
         RetrospectButtonRendererComponent,
         DeleteButtonRendererComponent,
         NumericCellEditorComponent
